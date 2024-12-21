1f947e4620399356ab769984c81b1859f5b365ef
Marco Churra <mchurra@outlook.pt>
2024-12-21 19:24:06 +0000

    Another fix to the projects object

--- ArcDevPortefolio/src/mappedInfo/pastProjects.tsx
+++ ArcDevPortefolio/src/mappedInfo/pastProjects.tsx
@@ -39,7 +39,7 @@ export const oldProjects: Project[] = [
     title: "Webflix",
     thumbnail: "/assets/projects/movie.lg.png",
     techStack: ["css", "js"],
-    src: "https://flixweb.netlify.app/",
+    src: "https://twimbaxclone.netlify.app/",
     github: "https://github.com/MarChurra/YourMovieWatchList",
   },
   {
